74e7fcfb9d0a6c7224fb2591f9c86c5f4ff93693
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-30 16:43:42 -0800

    Remove imports

--- jobly-frontend/src/SearchForm.js
+++ jobly-frontend/src/SearchForm.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import "./SearchForm.css";
 import { debounce } from "./utils";
 
